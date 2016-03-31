1f3680ca12d4f34b0d6e2753ae9636b9fb7795a0
ErikBoesen <me@erikboesen.com>
2016-03-31 19:10:03 -0400

    Live theme transition on NT page, fixed slightly broken zero trimming

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # BW New Tab Page
-![Screenshot](screen1.png)
-![Screenshot](screen2.png)
+![Screenshot](screen1.png) ![Screenshot](screen2.png)
 
 Black and White New Tab Page by [Erik Boesen](https://github.com/ErikBoesen).
 
@@ -13,6 +12,6 @@ Black and White New Tab Page by [Erik Boesen](https://github.com/ErikBoesen).
 
 This extension should work out of box. Settings can be configured in the Options link.
 
----
+--------------------------------------------------------------------------------
 
 This project is protected under Creative Commons 0. Basically, do whatever you want with it. I'd like if you gave me credit, but you don't _have_ to.
--- newtab.js
+++ newtab.js
@@ -1,26 +1,29 @@
 var clock = function() {
-    var h = new Date().getHours(),
-    m = new Date().getMinutes();
-    if (localStorage.format === '12') {
-        if (h > 12) {
-            h = h-12;
-        }
-    }
-    if (localStorage.zero === 'true') {
-        if (h < 10) {h = '0' + h;}
-        if (m < 10) {m = '0' + m;}
-    }
-    document.getElementById('hours').innerHTML = h;
-    document.getElementById('minutes').innerHTML = m;
-};
-addEventListener('load', function () {
-	if (localStorage.theme === 'dark') {
-		document.body.classList.remove('light');
-		document.body.classList.add('dark');
-	} else {
-		document.body.classList.remove('dark');
-		document.body.classList.add('light');
+	var h = new Date().getHours(),
+		m = new Date().getMinutes();
+	if (localStorage.format === '12') {
+		if (h > 12) {
+			h = h - 12;
+		}
 	}
-    clock();
-    setInterval(function() {clock();}, 200);
+	if (localStorage.zero === 'true') {
+		if (h < 10) { h = '0' + h; }
+	}
+    if (m < 10) { m = '0' + m; }
+	document.getElementById('hours').innerHTML = h;
+	document.getElementById('minutes').innerHTML = m;
+};
+
+addEventListener('load', function() {
+	clock();
+	setInterval(function() {
+        clock();
+		if (localStorage.theme === 'dark') {
+			document.body.classList.remove('light');
+			document.body.classList.add('dark');
+		} else {
+			document.body.classList.remove('dark');
+			document.body.classList.add('light');
+		}
+	}, 200);
 });
