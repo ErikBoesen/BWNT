d0f053a8db45f55d94ed25c77204aeb011a13f42
ErikBoesen <me@erikboesen.com>
2016-03-31 21:02:09 -0400

    Reimplemented night theme (white on black)

--- newtab.js
+++ newtab.js
@@ -10,20 +10,13 @@ var clock = function() {
 		if (h < 10) { h = '0' + h; }
 	}
     if (m < 10) { m = '0' + m; }
-	document.getElementById('hours').innerHTML = h;
-	document.getElementById('minutes').innerHTML = m;
+	document.getElementById('clock').innerHTML = h + ':' + m;
 };
 
 addEventListener('load', function() {
 	clock();
 	setInterval(function() {
         clock();
-		if (localStorage.theme === 'dark') {
-			document.body.classList.remove('light');
-			document.body.classList.add('dark');
-		} else {
-			document.body.classList.remove('dark');
-			document.body.classList.add('light');
-		}
+        document.body.className = localStorage.theme;
 	}, 200);
 });
--- options.js
+++ options.js
@@ -9,7 +9,6 @@ for (i = 0; i < format.children.length; i++) {
 		break;
 	}
 }
-
 for (i = 0; i < theme.children.length; i++) {
 	var child = theme.children[i];
 	if (child.value == localStorage.theme) {
@@ -17,7 +16,6 @@ for (i = 0; i < theme.children.length; i++) {
 		break;
 	}
 }
-
 for (i = 0; i < zero.children.length; i++) {
 	var child = zero.children[i];
 	if (child.value == localStorage.zero) {
@@ -31,11 +29,5 @@ setInterval(function() {
 	localStorage.theme = theme.children[theme.selectedIndex].value;
 	localStorage.zero = zero.children[zero.selectedIndex].value;
 
-	if (localStorage.theme === 'dark') {
-		document.body.classList.remove('light');
-		document.body.classList.add('dark');
-	} else {
-		document.body.classList.remove('dark');
-		document.body.classList.add('light');
-	}
+	document.body.className = localStorage.theme;
 }, 50);
