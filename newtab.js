32468f6e17210129a6706feabbdc414bc01078a4
ErikBoesen <me@erikboesen.com>
2016-04-01 18:06:26 -0400

    Added option to rotate theme by time of day.

--- newtab.js
+++ newtab.js
@@ -1,4 +1,4 @@
-var clock = function() {
+function clock() {
 	var h = new Date().getHours(),
 		m = new Date().getMinutes();
 	if (localStorage.format === '12') {
@@ -11,12 +11,19 @@ var clock = function() {
 	}
     if (m < 10) { m = '0' + m; }
 	document.getElementById('clock').innerHTML = h + ':' + m;
-};
+}
 
-addEventListener('load', function() {
+clock();
+setInterval(function() {
 	clock();
-	setInterval(function() {
-        clock();
-        document.body.className = localStorage.theme;
-	}, 200);
-});
+	if (JSON.parse(localStorage.cycle)) {
+		var hour = new Date().getHours();
+		if (hour >= 6 && hour <= 20) {
+			document.body.className = 'light';
+		} else {
+			document.body.className = 'night';
+		}
+	} else {
+		document.body.className = localStorage.theme;
+	}
+}, 200);
--- options.js
+++ options.js
@@ -1,6 +1,7 @@
 var format = document.getElementById('format'),
 	theme = document.getElementById('theme'),
-	zero = document.getElementById('zero');
+	zero = document.getElementById('zero'),
+	cycle = document.getElementById('cycle');
 
 for (i = 0; i < format.children.length; i++) {
 	var child = format.children[i];
@@ -17,11 +18,22 @@ for (i = 0; i < theme.children.length; i++) {
 	}
 }
 zero.checked = JSON.parse(localStorage.zero);
+cycle.checked = JSON.parse(localStorage.cycle);
 
 setInterval(function() {
 	localStorage.format = format.children[format.selectedIndex].value;
 	localStorage.theme = theme.children[theme.selectedIndex].value;
 	localStorage.zero = zero.checked;
+	localStorage.cycle = cycle.checked;
 
-	document.body.className = localStorage.theme;
+    if (cycle.checked) {
+        var hour = new Date().getHours();
+        if (hour >= 6 && hour <= 20) {
+            document.body.className = 'light';
+        } else {
+            document.body.className = 'night';
+        }
+    } else {
+        document.body.className = localStorage.theme;
+    }
 }, 50);
