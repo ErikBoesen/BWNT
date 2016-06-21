b9008dd160b81ec0d2fa3fc7f369de275cbb7ba2
ErikBoesen <me@erikboesen.com>
2016-06-21 12:16:32 -0400

    Arrays created at init now + code style improvements

--- newtab.js
+++ newtab.js
@@ -1,16 +1,20 @@
 if (localStorage.length != 5) {
-    localStorage.format = true;
-    localStorage.zero = false;
-    localStorage.showDate = true;
-    localStorage.cycle = true;
-    localStorage.theme = 'light';
+	localStorage.format = true;
+	localStorage.zero = false;
+	localStorage.showDate = true;
+	localStorage.cycle = true;
+	localStorage.theme = 'light';
 }
 
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 function update() {
-        
-    var date = new Date(), h = date.getHours(),
-		           m = date.getMinutes();
-    if (JSON.parse(localStorage.cycle)) {
+
+	var date = new Date(),
+		h = date.getHours(),
+		m = date.getMinutes();
+	if (JSON.parse(localStorage.cycle)) {
 		if (h >= 6 && h <= 20) {
 			document.body.className = 'light';
 		} else {
@@ -20,23 +24,17 @@ function update() {
 		document.body.className = localStorage.theme;
 	}
 	if (JSON.parse(localStorage.format)) {
-		if (h > 12) { h = h - 12; }
-        if (h === 0) { h = 12; }
+		if (h > 12) h -= 12;
+		if (h === 0) h = 12;
 	}
-	if (m < 10) { m = '0' + m; }
+	if (m < 10) m = '0' + m;
 	document.getElementById('clock').innerHTML = h + ':' + m;
-    if (JSON.parse(localStorage.showDate)) {
-        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        var mon = months[date.getMonth()];
-        
-        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var day = days[date.getDay()];
-       
-        document.getElementById('date').innerHTML = day + ', ' + mon + ' ' + date.getDate();
-    } else {
-        document.getElementById('date').innerHTML = '';
-    }
+	if (JSON.parse(localStorage.showDate)) {
+		document.getElementById('date').innerHTML = days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate();
+	} else {
+		document.getElementById('date').innerHTML = '';
+	}
 }
 
 update();
-setInterval(function() { update(); }, 200);
+setInterval(update(), 200);
