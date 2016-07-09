64ec9e4d973d2650e012c713e79061f824ca0051
ErikBoesen <me@erikboesen.com>
2016-07-08 20:23:51 -0400

    Commented JS

--- newtab.js
+++ newtab.js
@@ -1,3 +1,4 @@
+// If the localStorage (options) variables haven't been created yet, initialize them at default values.
 if (localStorage.length != 5) {
 	localStorage.format = true;
 	localStorage.zero = false;
@@ -6,35 +7,47 @@ if (localStorage.length != 5) {
 	localStorage.theme = 'light';
 }
 
+// Arrays of month and day names which will be chosen from to build date.
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 function update() {
-
-	var date = new Date(),
-		h = date.getHours(),
-		m = date.getMinutes();
+    // Get the current hour and minute.
+	var d = new Date(),
+		h = d.getHours(),
+		m = d.getMinutes();
+    // If automatic day/night theme cycle is turned on, do it.
+    // JSON.parse() must be used when fetching booleans; localStorage will return them as strings.
 	if (JSON.parse(localStorage.cycle)) {
-		if (h >= 6 && h <= 20) {
-			document.body.className = 'light';
-		} else {
-			document.body.className = 'night';
-		}
+        // If it's daytime, apply light theme, if nighttime, apply night theme.
+		document.body.className = h >= 6 && h <= 20 ? 'light' : 'night';
+    // Otherwise, just apply whichever theme the user has selected.
 	} else {
+        // Apply selected theme
 		document.body.className = localStorage.theme;
 	}
+    // If 12 hour time is turned on
 	if (JSON.parse(localStorage.format)) {
+        // Convert hours above 12 to 12-hour counterparts
 		if (h > 12) h -= 12;
+        // Correct for hour 0
 		if (h === 0) h = 12;
 	}
+    // If minutes are only one digit long, add a leading 0.
 	if (m < 10) m = '0' + m;
+    // Fill in clock
 	document.getElementById('clock').innerHTML = h + ':' + m;
+    // If date is desired to be shown
 	if (JSON.parse(localStorage.showDate)) {
-		document.getElementById('date').innerHTML = days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate();
+        // Fill in date
+		document.getElementById('date').innerHTML = days[d.getDay()] + ', ' + months[d.getMonth()] + ' ' + d.getDate();
 	} else {
+        // Delete date
 		document.getElementById('date').innerHTML = '';
 	}
 }
 
+// On page load, update for the first time
 update();
-setInterval(update(), 200);
+// Update again every 50 milliseconds
+setInterval(update, 200);
--- options.js
+++ options.js
@@ -1,28 +1,29 @@
+// Aliases for control elements
 var format = document.getElementById('format'),
     showDate = document.getElementById('showDate'),
 	cycle = document.getElementById('cycle'),
 	theme = document.getElementById('theme');
 
+// On page load, get current options from localStorage
 format.checked = JSON.parse(localStorage.format);
 showDate.checked = JSON.parse(localStorage.showDate);
 cycle.checked = JSON.parse(localStorage.cycle);
-for (i = 0; i < theme.children.length; i++) {
-	var child = theme.children[i];
-	if (child.value == localStorage.theme) {
-		child.selected = true;
-		break;
-	}
-}
+theme.value = localStorage.theme;
 
+// Every 50 milliseconds
 setInterval(function() {
+    // Update localStorage values if control elements have been modified.
 	localStorage.format = format.checked;
     localStorage.showDate = showDate.checked;
 	localStorage.cycle = cycle.checked;
 	localStorage.theme = theme.children[theme.selectedIndex].value;
 
-	if (cycle.checked) {
+    // If automatic day/night theme is on, mark theme selector as invalid by graying it out
+	if (localStorage.cycle) {
 		theme.parentElement.style.opacity = 0.5;
+    // Otherwise
 	} else {
+        // Make it fully opaque
 		theme.parentElement.style.opacity = 1;
 	}
 }, 50);
