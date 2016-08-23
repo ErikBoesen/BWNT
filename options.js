d52f99f8e665bfc275b77e10f79ab010a36418c0
Germán Franco <dev.germanfr@gmail.com>
2016-08-23 19:38:47 +0200

    [REVAMP] Improved perforance and code quality New features:  * Event driven settings. They won't reload every 50ms anymore! They wil    notify all running clocks that settings have changed.  * Object oriented clock. Cleaner and more readable code. Actions    splitted into parts.  * Read options from local objects. Repeated loads from localStorage    could potentially reducce performance.

--- options.js
+++ options.js
@@ -1,29 +1,52 @@
-// Aliases for control elements
-var format = document.getElementById('format'),
-    showDate = document.getElementById('showDate'),
-	cycle = document.getElementById('cycle'),
-	theme = document.getElementById('theme');
 
-// On page load, get current options from localStorage
-format.checked = JSON.parse(localStorage.format);
-showDate.checked = JSON.parse(localStorage.showDate);
-cycle.checked = JSON.parse(localStorage.cycle);
-theme.value = localStorage.theme;
+(function() {
 
-// Every 50 milliseconds
-setInterval(function() {
-    // Update localStorage values if control elements have been modified.
-	localStorage.format = format.checked;
-    localStorage.showDate = showDate.checked;
-	localStorage.cycle = cycle.checked;
-	localStorage.theme = theme.children[theme.selectedIndex].value;
-
-    // If automatic day/night theme is on, mark theme selector as invalid by graying it out
-	if (localStorage.cycle) {
-		theme.parentElement.style.opacity = 0.5;
-    // Otherwise
-	} else {
-        // Make it fully opaque
-		theme.parentElement.style.opacity = 1;
+	//Check whether config has been initialized locally
+	function check_init_config() {
+		localStorage.format = localStorage.format || false;
+		localStorage.show_date = localStorage.show_date || true;
+		localStorage.cycle = localStorage.cycle || true;
+		localStorage.theme = localStorage.theme || 'light';
 	}
-}, 50);
+
+	check_init_config();
+
+	document.addEventListener('DOMContentLoaded', function() {
+		var format = document.getElementById('format');
+		format.checked = JSON.parse(localStorage.format);
+		format.addEventListener('change',function() {
+			localStorage.format = format.checked;
+			clock._load_options();
+		});
+
+		var show_date = document.getElementById('showDate');
+		show_date.checked = JSON.parse(localStorage.show_date);
+		show_date.addEventListener('change',function() {
+			localStorage.show_date = show_date.checked;
+			clock._load_options();
+		});
+
+		var theme = document.getElementById('theme');
+		theme.value = localStorage.theme;
+		theme.addEventListener('change',function() {
+			localStorage.theme = theme.children[theme.selectedIndex].value;
+			clock._load_options();
+		});
+
+		var cycle = document.getElementById('cycle');
+		cycle.checked = JSON.parse(localStorage.cycle);
+		cycle.addEventListener('change',function() {
+			localStorage.cycle = cycle.checked;
+			clock._load_options();
+
+			// If automatic day/night theme is on, mark theme selector as invalid by graying it out
+			if (cycle.checked) {
+				theme.parentElement.style.opacity = 0.5;
+			} else {
+				theme.parentElement.style.opacity = 1;
+			}
+		});
+
+	});
+
+})();
