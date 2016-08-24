0c38d0ced18f8e52b1626ac9aaa38080d856b7ab
Germán Franco <dev.germanfr@gmail.com>
2016-08-25 00:00:27 +0200

    Disable theme picking on auto mode Needed to change some code location due to not already defined variables. Moved all getElement... to the top.

--- options.js
+++ options.js
@@ -13,38 +13,36 @@
 
 	document.addEventListener('DOMContentLoaded', function() {
 		var format = document.getElementById('format');
+		var show_date = document.getElementById('showDate');
+		var theme = document.getElementById('theme');
+		var cycle = document.getElementById('cycle');
+
 		format.checked = JSON.parse(localStorage.format);
 		format.addEventListener('change',function() {
 			localStorage.format = format.checked;
 			clock.load_options();
 		});
 
-		var show_date = document.getElementById('showDate');
 		show_date.checked = JSON.parse(localStorage.show_date);
 		show_date.addEventListener('change',function() {
 			localStorage.show_date = show_date.checked;
 			clock.load_options();
 		});
 
-		var theme = document.getElementById('theme');
-		theme.value = localStorage.theme;
-		theme.addEventListener('change',function() {
-			localStorage.theme = theme.children[theme.selectedIndex].value;
-			clock.load_options();
-		});
-
-		var cycle = document.getElementById('cycle');
 		cycle.checked = JSON.parse(localStorage.cycle);
 		cycle.addEventListener('change',function() {
 			localStorage.cycle = cycle.checked;
 			clock.load_options();
 
-			// If automatic day/night theme is on, mark theme selector as invalid by graying it out
-			if (cycle.checked) {
-				theme.parentElement.style.opacity = 0.5;
-			} else {
-				theme.parentElement.style.opacity = 1;
-			}
+			// Enable/disable theme picker if auto theming enabled
+			theme.disabled = cycle.checked;
+		});
+
+		theme.value = localStorage.theme;
+		theme.disabled = cycle.checked;
+		theme.addEventListener('change',function() {
+			localStorage.theme = theme.children[theme.selectedIndex].value;
+			clock.load_options();
 		});
 
 	});
