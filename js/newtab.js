1258693413d2be9fbc070e54dffaab9de716b819
ErikBoesen <me@erikboesen.com>
2017-11-11 13:35:24 -0500

    Firefox compatibility

--- js/newtab.js
+++ js/newtab.js
@@ -4,240 +4,244 @@
 */
 
 var newtab = (function() {
-	var extension = {};
+    var extension = {};
 
-	const REFRESH_TIME_MS = 1000;
-	const CLOCK_ID = 'clock';
+    const REFRESH_TIME_MS = 1000;
+    const CLOCK_ID = 'clock';
 
-	const THEME_DEFAULT = 'theme-light';
-	const THEME_DEFAULT_DARK = 'theme-night';
+    const THEME_DEFAULT = 'theme-light';
+    const THEME_DEFAULT_DARK = 'theme-night';
 
-	const KEYS = {
-		months : [
-			'month_january',
-			'month_february',
-			'month_march',
-			'month_april',
-			'month_may',
-			'month_june',
-			'month_july',
-			'month_august',
-			'month_september',
-			'month_october',
-			'month_november',
-			'month_december'
-		],
-		weekdays : [
-			'day_sunday',
-			'day_monday',
-			'day_tuesday',
-			'day_wednesday',
-			'day_thursday',
-			'day_friday',
-			'day_saturday'
-		],
-		date_format : 'date_format',
-		time_pm : 'time_pm',
-		time_am : 'time_am'
-	};
+    const KEYS = {
+        months: [
+            'month_january',
+            'month_february',
+            'month_march',
+            'month_april',
+            'month_may',
+            'month_june',
+            'month_july',
+            'month_august',
+            'month_september',
+            'month_october',
+            'month_november',
+            'month_december'
+        ],
+        weekdays: [
+            'day_sunday',
+            'day_monday',
+            'day_tuesday',
+            'day_wednesday',
+            'day_thursday',
+            'day_friday',
+            'day_saturday'
+        ],
+        date_format: 'date_format',
+        time_pm: 'time_pm',
+        time_am: 'time_am'
+    };
 
-	function _(key) {
-		var trans = chrome.i18n.getMessage(key);
+    function _(key) {
+        var trans = chrome.i18n.getMessage(key);
 
-		if (trans) return trans;
-		return key;
-	}
+        if (trans) return trans;
+        return key;
+    }
 
-	function get_meridiem(hour) {
-		return _(hour >= 12 ? KEYS.time_pm : KEYS.time_am);
-	}
+    function get_meridiem(hour) {
+        return _(hour >= 12 ? KEYS.time_pm : KEYS.time_am);
+    }
 
-	extension.read_option = function(name,callback) {
-		return chrome.storage.local.get(name, callback);
-	}
+    extension.read_option = function(name, callback) {
+        return chrome.storage.local.get(name, callback);
+    }
 
-	function Clock() {
-		Clock_init_DOM.call(this);
-		Clock_init_fields.call(this);
-		this.load_options();
-		this.start();
+    function Clock() {
+        Clock_init_DOM.call(this);
+        Clock_init_fields.call(this);
+        this.load_options();
+        this.start();
 
-		// Automatically update all tabs
-		window.addEventListener('storage', this.load_options.bind(this));
-		chrome.storage.onChanged.addListener(function(changes, namespace) {
-			if (changes.background_image) // If the update is background related
-				this.set_background_image(changes.background_image.newValue);
-		}.bind(this));
-	}
+        // Automatically update all tabs
+        window.addEventListener('storage', this.load_options.bind(this));
+        chrome.storage.onChanged.addListener(function(changes, namespace) {
+            if (changes.background_image) // If the update is background related
+                this.set_background_image(changes.background_image.newValue);
+        }.bind(this));
+    }
 
-	// Init clock fields at construction
-	function Clock_init_fields() {
-		this._last_theme = 'notheme';
-		this.bg_image = '';
-		// First time background load
-		extension.read_option('background_image', function(items) {
-			Clock_start_background_animation.call(this,items.background_image);
-			this.set_background_image(items.background_image);
-		}.bind(this));
+    // Init clock fields at construction
+    function Clock_init_fields() {
+        this._last_theme = 'notheme';
+        this.bg_image = '';
+        // First time background load
+        extension.read_option('background_image', function(items) {
+            Clock_start_background_animation.call(this, items.background_image);
+            this.set_background_image(items.background_image);
+        }.bind(this));
 
-		this.date_pattern = _(KEYS.date_format);
-	}
+        this.date_pattern = _(KEYS.date_format);
+    }
 
-	// Init DOM elements
-	function Clock_init_DOM() {
-		var clock_container = document.getElementById(CLOCK_ID);
+    // Init DOM elements
+    function Clock_init_DOM() {
+        var clock_container = document.getElementById(CLOCK_ID);
 
-		this._clock_elem = document.createElement('div');
-		clock_container.appendChild(this._clock_elem);
+        this._clock_elem = document.createElement('div');
+        clock_container.appendChild(this._clock_elem);
 
-		var ampm = document.createElement('div');
-		ampm.className = 'meridiem';
-		clock_container.appendChild(ampm);
-		this._meridiem_elem = ampm;
+        var ampm = document.createElement('div');
+        ampm.className = 'meridiem';
+        clock_container.appendChild(ampm);
+        this._meridiem_elem = ampm;
 
-		this._date_elem = document.getElementById('date');
-		this._bg_elem = document.getElementById('background-layer');
-	}
+        this._date_elem = document.getElementById('date');
+        this._bg_elem = document.getElementById('background-layer');
+    }
 
-	// Sync local options with localStorage
-	Clock.prototype.load_options = function() {
-		this.format = localStorage.format === 'true'; // Default: false
-		this.show_date = localStorage.show_date !== 'false'; // Default: true
-		this.cycle = localStorage.cycle !== 'false'; // Default: true
+    // Sync local options with localStorage
+    Clock.prototype.load_options = function() {
+        this.format = localStorage.format === 'true'; // Default: false
+        this.show_date = localStorage.show_date !== 'false'; // Default: true
+        this.cycle = localStorage.cycle !== 'false'; // Default: true
 
-		this.update();
-		if (this.cycle) {
-			this.theme = this.hour >= 6 && this.hour <= 20 ? THEME_DEFAULT : THEME_DEFAULT_DARK;
-		} else {
-			this.theme = localStorage.theme || THEME_DEFAULT;
-		}
-		this.use_bg_image = localStorage.use_bg_image === 'true';
+        this.update();
+        if (this.cycle) {
+            this.theme = this.hour >= 6 && this.hour <= 20 ? THEME_DEFAULT : THEME_DEFAULT_DARK;
+        } else {
+            this.theme = localStorage.theme || THEME_DEFAULT;
+        }
+        this.use_bg_image = localStorage.use_bg_image === 'true';
 
-		Clock_build.call(this);
-		this.show();
-	};
+        Clock_build.call(this);
+        this.show();
+    };
 
-	Clock.prototype.set_background_image = function(data) {
-		this.bg_image = data;
-		if (this.use_bg_image)
-			Clock_background_update.call(this);
-	};
+    Clock.prototype.set_background_image = function(data) {
+        this.bg_image = data;
+        if (this.use_bg_image)
+            Clock_background_update.call(this);
+    };
 
-	function Clock_background_update() {
-		var url = '';
-		if (this.use_bg_image) {
-			url = 'url(\'' + this.bg_image + '\')';
-		}
-		this._bg_elem.style.backgroundImage = url;
-		document.body.classList.toggle('bgimage',this.use_bg_image);
-	}
+    function Clock_background_update() {
+        var url = '';
+        if (this.use_bg_image) {
+            url = 'url(\'' + this.bg_image + '\')';
+        }
+        this._bg_elem.style.backgroundImage = url;
+        document.body.classList.toggle('bgimage', this.use_bg_image);
+    }
 
-	// Show the hidden contents when the image is loaded
-	// For dark unknown reasons, recieving the url as parameter is faster
-	// than getting it from 'this'. It just works.
-	function Clock_start_background_animation(url) {
-		if(!url) {
-			_make_visible.call(this);
-			return;
-		}
-		var img = new Image();
-		img.onload = _make_visible.bind(this);
-		img.src = url;
+    // Show the hidden contents when the image is loaded
+    // For dark unknown reasons, recieving the url as parameter is faster
+    // than getting it from 'this'. It just works.
+    function Clock_start_background_animation(url) {
+        if (!url) {
+            _make_visible.call(this);
+            return;
+        }
+        var img = new Image();
+        img.onload = _make_visible.bind(this);
+        img.src = url;
 
-		function _make_visible() {
-			this._bg_elem.style.opacity = '1';
-		}
-	}
+        function _make_visible() {
+            this._bg_elem.style.opacity = '1';
+        }
+    }
 
-	// Build clock from options like changing the theme, or hiding elements
-	function Clock_build() {
-		// Avoid unnecesary redraws
-		if (this._last_theme !== this.theme) {
-			document.body.classList.remove(this._last_theme);
-			document.body.classList.add(this.theme);
-			this._last_theme = this.theme;
-		}
+    // Build clock from options like changing the theme, or hiding elements
+    function Clock_build() {
+        // Avoid unnecesary redraws
+        if (this._last_theme !== this.theme) {
+            document.body.classList.remove(this._last_theme);
+            document.body.classList.add(this.theme);
+            this._last_theme = this.theme;
+        }
 
-		Clock_background_update.call(this);
+        Clock_background_update.call(this);
 
-		// Clear date view
-		if (!this.show_date)
-			this._date_elem.textContent = '';
+        // Clear date view
+        if (!this.show_date)
+            this._date_elem.textContent = '';
 
-		if (!this.format)
-			this._meridiem_elem.textContent = '';
-	}
+        if (!this.format)
+            this._meridiem_elem.textContent = '';
+    }
 
-	Clock.prototype.get_date_string = function(date) {
-		var month = _(KEYS.months[date.getMonth()]),
-			weekday = _(KEYS.weekdays[date.getDay()]),
-			day = date.getDate();
+    Clock.prototype.get_date_string = function(date) {
+        var month = _(KEYS.months[date.getMonth()]),
+            weekday = _(KEYS.weekdays[date.getDay()]),
+            day = date.getDate();
 
-		return this.date_pattern.replace(/{(month|day|weekday)}/g, function(match, key) {
-			switch (key) {
-				case 'month':   return month;
-				case 'day':     return day;
-				case 'weekday': return weekday;
-				default: match;
-			}
-		});
-	}
+        return this.date_pattern.replace(/{(month|day|weekday)}/g, function(match, key) {
+            switch (key) {
+                case 'month':
+                    return month;
+                case 'day':
+                    return day;
+                case 'weekday':
+                    return weekday;
+                default:
+                    match;
+            }
+        });
+    }
 
-	// Updates internal date and time to the current one
-	Clock.prototype.update = function() {
-		var date = new Date(),
-		    h = date.getHours(),
-        h12,
-		    m = date.getMinutes();
+    // Updates internal date and time to the current one
+    Clock.prototype.update = function() {
+        var date = new Date(),
+            h = date.getHours(),
+            h12,
+            m = date.getMinutes();
 
-		// Format hour to 12h
-		h12 = h % 12;
-		if (h12 === 0) h12 = 12;
+        // Format hour to 12h
+        h12 = h % 12;
+        if (h12 === 0) h12 = 12;
 
-		// Add trailing 0 for one digit numbers
-		this.minute = m < 10 ? '0' + m : m;
-		this.hour   = h < 10 ? '0' + h : h;
-		this.hour12 = h12;
-		this.meridiem = get_meridiem(h);
-		this.date = this.get_date_string(date);
-	};
+        // Add trailing 0 for one digit numbers
+        this.minute = m < 10 ? '0' + m : m;
+        this.hour = h < 10 ? '0' + h : h;
+        this.hour12 = h12;
+        this.meridiem = get_meridiem(h);
+        this.date = this.get_date_string(date);
+    };
 
-	// Fill in all visible widgets
-	Clock.prototype.show = function() {
-		var h;
-		if (this.format) {
-			h = this.hour12;
-			this._meridiem_elem.textContent = this.meridiem;
-		} else {
-			h = this.hour;
-		}
-		this._clock_elem.textContent = h + ':' + this.minute;
+    // Fill in all visible widgets
+    Clock.prototype.show = function() {
+        var h;
+        if (this.format) {
+            h = this.hour12;
+            this._meridiem_elem.textContent = this.meridiem;
+        } else {
+            h = this.hour;
+        }
+        this._clock_elem.textContent = h + ':' + this.minute;
 
-		if (this.show_date)
-			this._date_elem.textContent = this.date;
-	};
+        if (this.show_date)
+            this._date_elem.textContent = this.date;
+    };
 
-	// Starts clock running
-	Clock.prototype.start = function() {
-		var self = this;
-		if (!this._started_id) {
-			this._started_id = window.setInterval(function() {
-				self.update();
-				self.show();
-			}, REFRESH_TIME_MS);
-		}
-	};
+    // Starts clock running
+    Clock.prototype.start = function() {
+        var self = this;
+        if (!this._started_id) {
+            this._started_id = window.setInterval(function() {
+                self.update();
+                self.show();
+            }, REFRESH_TIME_MS);
+        }
+    };
 
-	// Stops clock from running
-	Clock.prototype.stop = function() {
-		if (this._started_id) {
-			window.clearInterval(this._started_id);
-			this._started_id = null;
-		}
-	};
+    // Stops clock from running
+    Clock.prototype.stop = function() {
+        if (this._started_id) {
+            window.clearInterval(this._started_id);
+            this._started_id = null;
+        }
+    };
 
-	extension.Clock = Clock;
-	return extension;
+    extension.Clock = Clock;
+    return extension;
 
 })();
 
--- js/options.js
+++ js/options.js
@@ -5,121 +5,121 @@
 
 (function(newtab) {
 
-	// Check whether config has been initialized locally
-	function check_init_config() {
-		// Note: 'Or' can be done against false values because they are strings
-		localStorage.format = localStorage.format || 'false';
-		localStorage.show_date = localStorage.show_date || 'true';
-		localStorage.cycle = localStorage.cycle || 'true';
-		localStorage.theme = localStorage.theme || 'theme-light';
+    // Check whether config has been initialized locally
+    function check_init_config() {
+        // Note: 'Or' can be done against false values because they are strings
+        localStorage.format = localStorage.format || 'false';
+        localStorage.show_date = localStorage.show_date || 'true';
+        localStorage.cycle = localStorage.cycle || 'true';
+        localStorage.theme = localStorage.theme || 'theme-light';
 
-		if(typeof localStorage.use_bg_image === 'undefined') {
-			// Init source field inside storage
-			save_to_storage('background_image', '');
-		}
-		localStorage.use_bg_image = localStorage.use_bg_image || 'false';
-	}
+        if (typeof localStorage.use_bg_image === 'undefined') {
+            // Init source field inside storage
+            save_to_storage('background_image', '');
+        }
+        localStorage.use_bg_image = localStorage.use_bg_image || 'false';
+    }
 
-	check_init_config();
+    check_init_config();
 
-	document.addEventListener('DOMContentLoaded', function() {
-		var format = document.getElementById('format'),
-			show_date = document.getElementById('showDate'),
-			theme = document.getElementById('theme'),
-			cycle = document.getElementById('cycle'),
-			use_background = document.getElementById('use-bg-img'),
-			bg_picker = document.getElementById('bg-img-picker');
+    document.addEventListener('DOMContentLoaded', function() {
+        var format = document.getElementById('format'),
+            show_date = document.getElementById('showDate'),
+            theme = document.getElementById('theme'),
+            cycle = document.getElementById('cycle'),
+            use_background = document.getElementById('use-bg-img'),
+            bg_picker = document.getElementById('bg-img-picker');
 
-		format.checked = localStorage.format === 'true';
-		format.addEventListener('change', function() {
-			localStorage.format = format.checked;
-			clock.load_options();
-		});
+        format.checked = localStorage.format === 'true';
+        format.addEventListener('change', function() {
+            localStorage.format = format.checked;
+            clock.load_options();
+        });
 
-		show_date.checked = localStorage.show_date === 'true';
-		show_date.addEventListener('change', function() {
-			localStorage.show_date = show_date.checked;
-			clock.load_options();
-		});
+        show_date.checked = localStorage.show_date === 'true';
+        show_date.addEventListener('change', function() {
+            localStorage.show_date = show_date.checked;
+            clock.load_options();
+        });
 
-		cycle.checked = localStorage.cycle === 'true';
-		cycle.addEventListener('change', function() {
-			localStorage.cycle = cycle.checked;
-			theme.disabled = cycle.checked;
-			clock.load_options();
-		});
+        cycle.checked = localStorage.cycle === 'true';
+        cycle.addEventListener('change', function() {
+            localStorage.cycle = cycle.checked;
+            theme.disabled = cycle.checked;
+            clock.load_options();
+        });
 
-		theme.value = localStorage.theme;
-		theme.disabled = cycle.checked;
-		theme.addEventListener('change', function() {
-			localStorage.theme = theme.children[theme.selectedIndex].value;
-			clock.load_options();
-		});
+        theme.value = localStorage.theme;
+        theme.disabled = cycle.checked;
+        theme.addEventListener('change', function() {
+            localStorage.theme = theme.children[theme.selectedIndex].value;
+            clock.load_options();
+        });
 
-		use_background.checked = localStorage.use_bg_image === 'true';
-		bg_picker.disabled = !use_background.checked;
-		use_background.addEventListener('change', function() {
-			localStorage.use_bg_image = use_background.checked;
-			bg_picker.disabled = !use_background.checked;
-			clock.load_options();
-		});
+        use_background.checked = localStorage.use_bg_image === 'true';
+        bg_picker.disabled = !use_background.checked;
+        use_background.addEventListener('change', function() {
+            localStorage.use_bg_image = use_background.checked;
+            bg_picker.disabled = !use_background.checked;
+            clock.load_options();
+        });
 
-		bg_picker.placeholder = chrome.i18n.getMessage('select_image');
-		bg_picker.addEventListener('change', function() {
-			var selected = bg_picker.files[0];
-			if (selected) {
-				toDataURL(selected, function(result) {
-					save_to_storage('background_image',result);
-				});
-			}
-		});
+        bg_picker.placeholder = chrome.i18n.getMessage('select_image');
+        bg_picker.addEventListener('change', function() {
+            var selected = bg_picker.files[0];
+            if (selected) {
+                toDataURL(selected, function(result) {
+                    save_to_storage('background_image', result);
+                });
+            }
+        });
 
-		enable_file_pickers();
+        enable_file_pickers();
 
-	}); // End of DOMContentLoaded
+    }); // End of DOMContentLoaded
 
-	// Convert data to base-64 asynchronously
-	function toDataURL(data, callback) {
-		if (!toDataURL.freader)
-			toDataURL.freader = new FileReader();
+    // Convert data to base-64 asynchronously
+    function toDataURL(data, callback) {
+        if (!toDataURL.freader)
+            toDataURL.freader = new FileReader();
 
-		var _internal_cb;
-		toDataURL.freader.readAsDataURL(data);
-		toDataURL.freader.addEventListener('load', _internal_cb = function() {
-			this.removeEventListener('load', _internal_cb);
-			callback(this.result);
-		});
-	}
+        var _internal_cb;
+        toDataURL.freader.readAsDataURL(data);
+        toDataURL.freader.addEventListener('load', _internal_cb = function() {
+            this.removeEventListener('load', _internal_cb);
+            callback(this.result);
+        });
+    }
 
-	// Save data into chrome storage
-	function save_to_storage(name, data) {
-		var obj = {};
-		obj[name] = data;
-		chrome.storage.local.set(obj);
-	}
+    // Save data into chrome storage
+    function save_to_storage(name, data) {
+        var obj = {};
+        obj[name] = data;
+        chrome.storage.local.set(obj);
+    }
 
-	// Add proper style and functionality to file inputs
-	function enable_file_pickers() {
-		var inputs = document.querySelectorAll('[type=\"file\"]');
-		var label, curr_input;
+    // Add proper style and functionality to file inputs
+    function enable_file_pickers() {
+        var inputs = document.querySelectorAll('[type=\"file\"]');
+        var label, curr_input;
 
-		// Change visible title on change
-		function _update_value() {
-			if (this.value)
-				this.label_elem.textContent = this.files[0].name;
-			else
-				this.label_elem.textContent = this.placeholder;
-		}
+        // Change visible title on change
+        function _update_value() {
+            if (this.value)
+                this.label_elem.textContent = this.files[0].name;
+            else
+                this.label_elem.textContent = this.placeholder;
+        }
 
-		for (var i = 0, len = inputs.length; i < len; i++) {
-			curr_input = inputs[i];
-			label = document.createElement('label');
-			label.textContent = curr_input.placeholder;
-			label.setAttribute('for', curr_input.id);
-			curr_input.parentNode.insertBefore(label, curr_input.nextSibling);
-			curr_input.addEventListener('change', _update_value.bind(curr_input));
-			curr_input.label_elem = label;
-		} // End of for-loop
-	}
+        for (var i = 0, len = inputs.length; i < len; i++) {
+            curr_input = inputs[i];
+            label = document.createElement('label');
+            label.textContent = curr_input.placeholder;
+            label.setAttribute('for', curr_input.id);
+            curr_input.parentNode.insertBefore(label, curr_input.nextSibling);
+            curr_input.addEventListener('change', _update_value.bind(curr_input));
+            curr_input.label_elem = label;
+        } // End of for-loop
+    }
 
 })(newtab);
